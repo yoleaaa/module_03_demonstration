62db0b77d6734de7acb1b27dbab216772fd1c753
Yuan Olea <yolea@academic.rrc.ca>
2024-01-30 12:27:54 -0600

    added save

---
+++ module_3_logic.py
@@ -0,0 +1,54 @@
+"""
+Author:Yuan Olea
+Description:  intro to python control structures
+date: 30 jan 2024
+"""
+
+
+#Sequintial, Selection, Iteration
+
+#SELCTION
+#Conditionals
+#if something is the case, do one thing, otherwise, do another thing
+
+age = 20
+#comparision evaluation
+age == 0
+print("not born yet")
+# Boolean ; true or false 
+if age < 13: 
+    #age is less than thirteen meaning
+    print("Child")
+
+
+    if True:
+    print("its true")
+
+#if (value that is true):
+    # run code
+    if False:
+    
+
+
+
+#Dynamic Code
+
+
+"""
+Vending machine app
+
+change = [5,10,25,100,200]
+price = 1.25
+input = 1.75
+
+calculate automatically how many pieces of change must be returned 
+look at change denominations available
+calculate how many of each are required and return them
+
+if difference = 100, return 100
+if difference - 50, return 2 x 25
+if the difference = 25, return 25
+
+
+
+"""
